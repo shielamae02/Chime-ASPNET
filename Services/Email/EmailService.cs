0c1e2d20f016f7439cc37beed51876c8b0073dd9
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:39:19 +0800

    chore: add retry logic to EmailService for handling email operation failures

--- Services/Email/EmailService.cs
+++ Services/Email/EmailService.cs
@@ -12,5 +12,15 @@ public class EmailService(
     SMTPSettings smtp
 ) : IEmailService
 {
-
+    private readonly static AsyncRetryPolicy RetryPolicy = Policy
+            .Handle<Exception>()
+            .WaitAndRetryAsync(3,
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                (exception, timeSpan, retryCount, _) =>
+                {
+                    Console.WriteLine(
+                        $"Retry {retryCount} encountered an error: {exception.Message}. Waiting {timeSpan} before next retry."
+                    );
+                }
+            );
 }
