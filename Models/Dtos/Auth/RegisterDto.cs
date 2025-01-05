4dab4a6eb9d3475643c0bf06068558e461179a2a
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:22:05 +0800

    chore: add RegisterDto for user registration data transfer

---
+++ Models/Dtos/Auth/RegisterDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Chime_ASPNET.Models.Dtos.Auth;
+
+public class RegisterDto : BaseUserDto
+{
+    [Required(ErrorMessage = "Password is required.")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters.")]
+    [DataType(DataType.Password)]
+    public string Password { get; init; } = string.Empty;
+}
