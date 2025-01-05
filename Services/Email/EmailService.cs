6ad8342d1670cbab877628517ab3b4c7cd251a61
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:39:27 +0800

    chore: implement SendEmailAsync method in EmailService for sending emails with retry logic

--- Services/Email/EmailService.cs
+++ Services/Email/EmailService.cs
@@ -23,4 +23,53 @@ public class EmailService(
                     );
                 }
             );
+
+    public async Task<bool> SendEmailAsync(IEnumerable<string> emails, string subject, string content)
+    {
+        logger.LogInformation("Starting email notification process.");
+        var emailList = emails.ToList();
+
+        if (emailList.Count == 0)
+        {
+            logger.LogWarning("No recipient email addresses provided. Email notification process aborted.");
+            return false;
+        }
+
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Chime", smtp.Username));
+
+        foreach (var email in emailList)
+        {
+            message.To.Add(new MailboxAddress("", email));
+            logger.LogInformation("Queued email to recipient: {email}", email);
+        }
+
+        message.Subject = subject;
+
+        var htmlBody = EmailTemplate.ForgotPasswordTemplate(subject, content);
+        message.Body = new TextPart("html") { Text = htmlBody };
+
+
+        var result = await RetryPolicy.ExecuteAsync(async () =>
+        {
+            using var client = new SmtpClient();
+            try
+            {
+                await client.ConnectAsync(smtp.Server, smtp.Port, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(smtp.Username, smtp.Password);
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+
+                logger.LogInformation("Email sent successfully.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation(ex, "Failed to send email.");
+                return false;
+            }
+        });
+
+        return result;
+    }
 }
