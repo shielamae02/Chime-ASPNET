03553fffb6340924b6ed42655e1b63ff3a6a4739
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:00:38 +0800

    chore: add AuthProfile for mapping between DTOs and User entity

---
+++ MappingProfiles/AuthProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using Chime_ASPNET.Models.Dtos.Auth;
+using Chime_ASPNET.Models.Entities;
+
+namespace Chime_ASPNET.MappingProfiles;
+
+public class AuthProfile : Profile
+{
+    public AuthProfile()
+    {
+        CreateMap<RegisterDto, User>();
+        CreateMap<LoginDto, User>();
+    }
+}
