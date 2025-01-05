0c9a7f1099bb349495ff8be7d5c7d8d410a31845
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 16:54:46 +0800

    chore: add DataContext class for Entity Framework Core integration

---
+++ Data/DataContext.cs
@@ -0,0 +1,33 @@
+using Chime_ASPNET.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chime_ASPNET.Data;
+
+public partial class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; init; }
+    public DbSet<Token> Tokens { get; init; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+        foreach (var entry in entries)
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+                    break;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+}
