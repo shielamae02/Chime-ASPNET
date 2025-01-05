5ee15c3cfdf74a64d089f6defee2039d0623c192
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:46:12 +0800

    chore: add OAuthCallbackAsync method to AuthService for handling OAuth login and registration

--- Services/Auth/AuthService.cs
+++ Services/Auth/AuthService.cs
@@ -24,5 +24,27 @@ public class AuthService(
     EmailQueue emailQueue
 ) : IAuthService
 {
+    #region OauthCallback
+    public async Task<ApiResponse<AuthDto>> OAuthCallbackAsync(OAuthDto request)
+    {
+        var isUserExist = await authRepository.IsUserExistByCredentialAsync(
+            request.Email,
+            request.ProviderId,
+            provider: request.Provider);
 
+        if (isUserExist)
+        {
+            logger.LogInformation("Proceeding with login for email: {Email}, provider: {Provider}", request.Email, request.Provider);
+            return await LoginAsync(
+                request,
+                req => authRepository.GetUserByCredentialsAsync(request.Email, request.ProviderId, request.Provider)
+            );
+        }
+
+        logger.LogInformation("Proceeding with registration for email: {Email}, provider: {Provider}", request.Email, request.Provider);
+        return await RegisterAsync(request, null);
+    }
+    #endregion
+
+    
 }
