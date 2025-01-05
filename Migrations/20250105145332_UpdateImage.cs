4d537f3230d4a366dbc271d6631165c09b65ed59
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 22:54:26 +0800

    chore: rename ProfilePicture column to Image in Users table

---
+++ Migrations/20250105145332_UpdateImage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chime_ASPNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePicture",
+                table: "Users",
+                newName: "Image");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Users",
+                newName: "ProfilePicture");
+        }
+    }
+}
