a7409c3e4e2d568d266a8739d934bf272a65f63c
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:34:03 +0800

    chore: add ControllerUtil class for utility functions in controllers

---
+++ Controllers/Utils/ControllerUtil.cs
@@ -0,0 +1,12 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Chime_ASPNET.Models.Response;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using static Chime_ASPNET.Models.Utils.Error;
+
+namespace Chime_ASPNET.Controllers.Utils;
+
+public static class ControllerUtil
+{
+
+}
