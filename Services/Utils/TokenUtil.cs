6462fb952d910cf6ef000ae61428cea47a18c6c0
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:31:59 +0800

    chore: add TokenUtil for generating JWT tokens with configurable expiry

---
+++ Services/Utils/TokenUtil.cs
@@ -0,0 +1,14 @@
+using System.Text;
+using System.Security.Claims;
+using Chime_ASPNET.Models.Config;
+using Chime_ASPNET.Models.Entities;
+using Chime_ASPNET.Models.Dtos.Auth;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Chime_ASPNET.Services.Utils;
+
+public static class TokenUtil
+{
+
+}
