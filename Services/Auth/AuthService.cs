f8b526a70fff1b63dde6be3749b4bc2fc160578f
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:47:39 +0800

    chore: add RefreshUserTokenAsync method to AuthService for token refresh functionality

--- Services/Auth/AuthService.cs
+++ Services/Auth/AuthService.cs
@@ -66,6 +66,67 @@ public class AuthService(
     }
     #endregion
 
+    #region RefreshUserToken
+    public async Task<ApiResponse<TokenDto>> RefreshUserTokenAsync(RefreshTokenDto request)
+    {
+        var validationErrors = new Dictionary<string, string>();
+
+        var principal = TokenUtil.ValidateToken(request.Refresh, jwt, env);
+        if (principal is null)
+        {
+            validationErrors.Add("token", "Invalid refresh token.");
+            return ApiResponse<TokenDto>.ErrorResponse(
+                Error.ValidationError,
+                Error.ErrorType.ValidationError,
+                validationErrors
+            );
+        }
+
+        await using var transaction = await context.Database.BeginTransactionAsync();
+        try
+        {
+            var token = await authRepository.GetTokenByRefreshAsync(request.Refresh);
+            if (token is null || token.IsRevoked || token.ExpiresAt < DateTime.UtcNow)
+            {
+                validationErrors.Add("token", "Refresh token is already expired or invalid.");
+                return ApiResponse<TokenDto>.ErrorResponse(
+                    Error.Unauthorized, Error.ErrorType.Unauthorized, validationErrors);
+            }
+
+            if (!TokenUtil.IsTokenNearExpiration(principal, bufferMinutes: 10))
+            {
+                var newAccessToken = new TokenDto
+                {
+                    Refresh = request.Refresh,
+                    Access = TokenUtil.GenerateToken(
+                        token.User, jwt, Token.TokenType.Access
+                    )
+                };
+
+                return ApiResponse<TokenDto>.SuccessResponse(Success.IS_AUTHENTICATED, newAccessToken);
+            }
+
+            token.IsRevoked = true;
+            await context.SaveChangesAsync();
+
+            var tokens = await CreateAndSaveTokensAsync(token.User);
+            await transaction.CommitAsync();
+
+            return ApiResponse<TokenDto>.SuccessResponse(Success.IS_AUTHENTICATED, tokens);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An unexpected error occurred in the refresh token service.");
+
+            await transaction.RollbackAsync();
+            return ApiResponse<TokenDto>.ErrorResponse(
+                Error.ServerError,
+                Error.ErrorType.InternalServerError
+            );
+        }
+    }
+    #endregion
+
 
 
 
