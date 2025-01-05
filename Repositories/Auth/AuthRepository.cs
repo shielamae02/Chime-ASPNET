9f54a1f7872f67fa4371b0d6a15bcba3eb3c12d6
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:40:58 +0800

    chore: add IsUserExistByCredentialAsync method to AuthRepository for user existence check

--- Repositories/Auth/AuthRepository.cs
+++ Repositories/Auth/AuthRepository.cs
@@ -6,6 +6,19 @@ namespace Chime_ASPNET.Repositories.Auth
 {
     public class AuthRepository(DataContext context) : IAuthRepository
     {
+        public async Task<bool> IsUserExistByCredentialAsync(
+                    string email,
+                    string? username = null,
+                    string? providerId = null,
+                    User.AuthenticationType? provider = null
+                )
+        {
+            return await context.Users
+                 .Where(u => (u.Email == email || u.Username == username)
+                    && (providerId == null || u.ProviderId == providerId)
+                    && (provider == null || u.Provider == provider))
+                .AnyAsync();
+        }
 
     }
 }
