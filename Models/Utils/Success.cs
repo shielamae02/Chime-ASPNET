950ea74428954c31221a29b4a6673ba467068976
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:23:24 +0800

    chore: add Success utility class for standardized success messages

---
+++ Models/Utils/Success.cs
@@ -0,0 +1,8 @@
+namespace Chime_ASPNET.Models.Utils;
+
+public static class Success
+{
+    public const string IS_AUTHENTICATED = "User has been successfully authenticated.";
+    public const string PASSWORD_RESET_LINK_SENT = "A password reset link was sent to the email address you provided. Please check your inbox (and spam folder, just in case) and follow the instructions to reset your password.";
+
+}
