bf6186988d78067f69dee337065f6269de894a2e
shielamae02 <shiela.mlepon@gmail.com>
2025-01-05 17:34:17 +0800

    chore: add GetUserId method to ControllerUtil for retrieving user ID from ClaimsPrincipal

--- Controllers/Utils/ControllerUtil.cs
+++ Controllers/Utils/ControllerUtil.cs
@@ -8,5 +8,11 @@ namespace Chime_ASPNET.Controllers.Utils;
 
 public static class ControllerUtil
 {
+    public static int GetUserId(ClaimsPrincipal user)
+    {
+        var userIdString = user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        return int.TryParse(userIdString, out var userId) ? userId : -1;
+    }
 
 }
